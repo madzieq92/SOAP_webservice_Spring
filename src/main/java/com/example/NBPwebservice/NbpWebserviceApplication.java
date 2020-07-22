8b06afc0aba1481cd0d1372f99393f50b524daac
Magdalena Peplowska <madzieq92@gmail.com>
2020-07-22 23:11:29 +0200

    Initial commit

---
+++ src/main/java/com/example/NBPwebservice/NbpWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.NBPwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbpWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NBPwebservice/NbpWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NBPwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
